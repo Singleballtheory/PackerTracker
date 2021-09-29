af83a80f16c7c72356c49b40bfdd8e19ded10bb1
Scott Hutley <scotthutley1@comcast.net>
2021-09-29 11:23:37 -0700

    Create initial framework

---
+++ PackerTracker/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PackerTracker.Controllers
+{
+  public class HomeController : Controller
+  {
+    public string Hello() { return "Hello friend!"; }
+  }  
+}
